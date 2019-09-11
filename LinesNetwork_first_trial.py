ed42f4c87b6c81a7dbe4ab464a905f51d920bfa5
LineKruse <30391510+LineKruse@users.noreply.github.com>
2019-09-11 12:40:21 +0200

    Add files via upload

---
+++ LinesNetwork_first_trial.py
@@ -0,0 +1,26 @@
+
+#Using scripts and data from Nielsen (2015) book
+
+import mnist_loader
+
+#Import data 
+ining_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+#Import network 
+import network
+
+#Define parameters in the network 
+net = network.Network([784, 30, 10]) 
+#784 input neurons (because the image has 784 pixels) 
+#30 hidden neurons 
+#10 output neurons (becuase it is trying to classify numbers, and there has to be a neuron for each possible output value)
+
+#Train networks 
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+#30 epochs 
+#mini-batches of size 10 
+#learning rate of 3.0
+
+#klsædjfælksjd 
+
+
